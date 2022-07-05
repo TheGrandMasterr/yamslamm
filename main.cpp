87efe3d4f404c231602f6c928daf279d0554c929
Matthew Zepf <58193861+TheGrandMasterr@users.noreply.github.com>
2022-07-05 13:04:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -94,23 +94,16 @@ void bestExpectedValue(Player p, Board b, int NumberofRollsLeft, vector<int> &ma
 }
 
 int main() {
-    int arr[5] = { 0,1,2,3,4 };
-    const int r = 3;
-    vector<vector<int>> combonations = printCombination(arr, 5, r);
-    return 0;
+    //int arr[5] = { 0,1,2,3,4 };
+    //const int r = 3;
+    //vector<vector<int>> combonations = printCombination(arr, 5, r);
+    //return 0;
+    Board b;
+    b.defualtBoard();
+    string name = "Matthew";
+    Player p(name);
+    while (p.doMove(b));
     // first create board, set to defualt, then make players
     // decide which player goes first and go to the left
     // play until all chips are gone then print out score and delete any objects
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
